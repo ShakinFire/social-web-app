c3d2dbea5b5337ca5a6becef125c9436687c0e96
Simeon <simeonsm@mail.ru>
2018-03-19 12:22:35 +0200

    fixed database name

--- db/config/config.js
+++ db/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: laniakea_db,
+    database: 'laniakea_db',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
