9dd1610de195e13773b8dd75ee1e90f29b397412
DESKTOP-E0R3LIQ\Tsvetelina <georgienevv@gmail.com>
2018-03-20 12:59:10 +0200

    updated fs travers routes

--- app/routes/routes-setup.js
+++ app/routes/routes-setup.js
@@ -1,7 +1,20 @@
+/* globals __dirname __filename */
+const fs = require('fs');
+const path = require('path');
+
 const init = (app) => {
     app.get('/', (req, res) => {
         res.render('fullscreen-video');
     });
+
+    fs.readdirSync(__dirname)
+        .filter((filename) => filename !== path.basename(__filename))
+        .filter((filename) => filename !== 'index.js')
+        .map((filename) => path.join(__dirname, filename))
+        .forEach((modulePath) => {
+            const route = require(modulePath);
+            route.init(app);
+        });
 };
 
 module.exports = {
